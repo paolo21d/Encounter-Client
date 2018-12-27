71798b07b8050a44773b6b8a485a4782b667a29c
paolo21d <paolo21d@gmail.com>
2018-12-27 17:17:10 +0100

    Rozdzielenie News na 3

--- Encounter-Client/News.cpp
+++ Encounter-Client/News.cpp
@@ -2,56 +2,77 @@
 
 using namespace std;
 
-ostream& operator<<(ostream& os, const News& x) {
-	if(x.gameMode == 1)
-		os << "FIGHT " <<  x.firstFighterId << " " << x.secondFighterId << endl;
-	else if(x.gameMode == 2)
-		os << "TRADE " << x.dealerFactor << endl;
-	else if(x.gameMode == 0)
+ostream& operator<<(ostream& os, const NewsExplore& x) {
+	if (x.gameMode == FIGHT)
+		os << "FIGHT " /*<<  x.firstFighter->getId() << " " << x.secondFighter->getId()*/ << endl;
+	else if (x.gameMode == DEAL)
+		os << "TRADE " <</* x.dealerFactor <<*/ endl;
+	else
 		os << "EXPLO" << endl;
 	cout << "sądziedzi: ";
-	for(int i: {0, 1, 2, 3}) cout << " " << x.adjacent[i];
-	cout << endl << "zarobiłem: " << x.income << endl << "pozycja: " << x.positionX << x.positionY << endl << endl;
-	
+	for (int i : {0, 1, 2, 3}) cout << " " << x.adjacent[i];
+	cout << endl/* << "zarobiłem: " << x.income << endl*/ << "pozycja: " << x.positionX << x.positionY << endl << endl;
+
 	return os;
 }
 
-sf::Packet& operator<<(sf::Packet& pckt, const News& x){
+sf::Packet& operator<<(sf::Packet& pckt, const NewsExplore& x) {
 	pckt << x.gameMode;
-	for(int i: {0, 1, 2, 3}) pckt << x.adjacent[i];
+	for (int i : {0, 1, 2, 3}) pckt << x.adjacent[i];
 	pckt << x.positionX << x.positionY;
 
 	return pckt;
 }
 
-sf::Packet& operator>>(sf::Packet& pckt, News& x){
-	int temp;
-	pckt >> temp;
- 	x.gameMode = static_cast<Mode>(temp);
-	for(int i: {0, 1, 2, 3}) pckt >> x.adjacent[i];
-	pckt >> x.positionX >> x.positionY;
-
+sf::Packet & operator >> (sf::Packet & pckt, NewsExplore & x)
+{
+	int mode;
+	pckt >> mode;
+	x.gameMode = static_cast <Mode> (mode);
+	/*if (mode == 0) {
+	x.gameMode = EXPLORE;
+	} else if (mode == 1) {
+	x.gameMode = FIGHT;
+	} else if (mode == 2) {
+	x.gameMode = DEAL;
+	}*/
+	for (int i : {0, 1, 2, 3}) pckt >> x.adjacent[i];
+	pckt >> x.positionX;
+	pckt >> x.positionY;
 	return pckt;
 }
 
-News::News(){
-	gameMode = EXPLORE;
-	firstFighterId = -1;
-	secondFighterId = -1;
-	dealerFactor = 0;	
-	income = 0;
-	for(int i:{1,2,3,4}){
-		adjacent[i] = 0;
-	}
-	positionX = positionY = 0;
+sf::Packet & operator<<(sf::Packet & pckt, const NewsDeal & x)
+{
+	// TODO: insert return statement here
+	return pckt;
 }
 
-void News::reset(){
-	gameMode = EXPLORE;
-	firstFighterId = -1;
-	secondFighterId = -1;
+sf::Packet & operator >> (sf::Packet & pckt, NewsDeal & x)
+{
+	// TODO: insert return statement here
+	return pckt;
+}
 
-	dealerFactor = 0;	
-	
-	income = 0;
+sf::Packet & operator<<(sf::Packet & pckt, const NewsFight & x)
+{
+	// TODO: insert return statement here
+	return pckt;
+}
+
+sf::Packet & operator >> (sf::Packet & pckt, NewsFight & x)
+{
+	// TODO: insert return statement here
+	return pckt;
+}
+
+
+NewsExplore::NewsExplore(Mode mode = EXPLORE, int posX = 0, int posY = 0) {
+	gameMode = mode;
+	for (int i : {1, 2, 3, 4}) {
+		adjacent[i] = 0;
+	}
+	//positionX = positionY = 0;
+	positionX = posX;
+	positionY = posY;
 }
--- Encounter-Client/News.h
+++ Encounter-Client/News.h
@@ -2,29 +2,42 @@
 #define NEWS_H
 #include <iostream>
 #include <fstream>
+#include "Character.h"
+#include "Deck.h"
 #include "SFML/Network.hpp"
 
 
-enum Mode{EXPLORE, FIGHT, DEAL};
+enum Mode { EXPLORE, FIGHT, DEAL };
 
-struct News{
+struct NewsExplore {
+	NewsExplore(Mode mode = EXPLORE, int posX = 0, int posY = 0);
 
-	Mode gameMode;
-	int firstFighterId;
-	int secondFighterId;
+	/*const Character* firstFighter; //przerzucić do klasy game
+	const Character* secondFighter;*/
+	//int income;
+	Mode gameMode; //w obie strony
+	int adjacent[4]; //0-góra, 1-prawa, 2-dół, 3-lewa //0-mozna wejsc, 1-pole nieinteraktywne, 2-pole interaktywne //wysylane z serwera do klienta
+	int positionX, positionY; //wysylane od klienta do serwera
 
-	double dealerFactor;	
-	
-	int income;
-	bool adjacent[4];			//0-góra, 1-prawa, 2-dół, 3-lewa
-	int positionX, positionY;
-
-	void reset();
-	News();
-
-	friend std::ostream& operator<<(std::ostream& os, const News& x);
-	friend sf::Packet& operator<<(sf::Packet& pckt, const News& x);
-	friend sf::Packet& operator>>(sf::Packet& pckt, News& x);
+	friend std::ostream& operator<<(std::ostream& os, const NewsExplore& x);
+	friend sf::Packet& operator<<(sf::Packet& pckt, const NewsExplore& x);
+	friend sf::Packet& operator >> (sf::Packet& pckt, NewsExplore& x);
 };
 
+struct NewsDeal {
+	Mode gameMode;
+	double dealerFactor;
+	//Deck cardsForSale; //mozliwe ze zamienic na vector intow czyli id kart
+	std::vector <int> cardsId;
+
+	friend sf::Packet& operator<<(sf::Packet& pckt, const NewsDeal& x);
+	friend sf::Packet& operator >> (sf::Packet& pckt, NewsDeal& x);
+};
+
+struct NewsFight {
+	Mode gameMode;
+
+	friend sf::Packet& operator<<(sf::Packet& pckt, const NewsFight& x);
+	friend sf::Packet& operator >> (sf::Packet& pckt, NewsFight& x);
+};
 #endif
